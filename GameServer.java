38d1463f87ccef86e141c98120c6c091298b635a
iel2408 <161427873+iel2408@users.noreply.github.com>
2024-05-11 00:29:21 +0800

    gameserver

--- GameServer.java
+++ GameServer.java
@@ -1,3 +1,38 @@
+import java.io.*;
+import java.net.*;
+
 public class GameServer {
-    // main method ng server
+    
+    private ServerSocket ss;
+    private int numPlayers;
+
+    public GameServer() {
+        numPlayers = 0;
+        try {
+            ss = new ServerSocket(85632);
+        } catch (IOException ex) {
+            System.out.println("IOException from GameServer Constructor");
+        }
+    }
+
+    public void acceptConnections() {
+        try {
+            System.out.println("Waiting for connections...");
+            while (numPlayers < 2) {
+                Socket s = ss.accept();
+                numPlayers++;
+                System.out.println("Player #" + numPlayers + " has connected.");
+            }
+
+            System.out.println("All players connected.");
+        } catch (IOException ex) {
+            System.out.println("IOException from acceptConnections()");
+        }
+    }
+
+    public static void main(String[] args) {
+        GameServer gs = new GameServer();
+        gs.acceptConnections();
+    }
+
 }
