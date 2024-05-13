2002478cca5f1b2766cdfdf3e0aeaaf96ad84888
iel2408 <161427873+iel2408@users.noreply.github.com>
2024-05-14 00:11:05 +0800

    test

---
+++ UserInterface.java
@@ -0,0 +1,39 @@
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import javax.imageio.*;
+import javax.swing.*;
+
+public class UserInterface extends JComponent {
+    
+    private BufferedImage image;
+    private int x, y;
+    private Image img;
+    private String fileName;
+
+    public UserInterface(String fileName, int x, int y, int width, int height) {
+        
+        this.fileName = fileName;
+        this.x = x;
+        this.y = y;
+        importImg();
+        img = image.getScaledInstance(width, height, Image.SCALE_DEFAULT);
+    }
+
+    private void importImg() {
+        InputStream is = getClass().getResourceAsStream(fileName);
+        try {
+            image = ImageIO.read(is);
+        } catch(Exception e) {
+            System.out.println("Exception at imporImg");
+        }
+    }
+
+    public void draw(Graphics2D g2d) {
+        g2d.drawImage(img, x, y, null);
+        
+    }
+
+}
